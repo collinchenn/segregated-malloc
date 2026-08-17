f2dc3f53a71431b58f2b0d34392ae581a7e19dec
Collin Chen <collic4@uci.edu>
2026-08-17 16:56:37 -0700

    heap functions

--- src/heap.c
+++ src/heap.c
@@ -1,17 +1,31 @@
+#include <sys/mman.h>
+
 #include "heap.h"
 
-static void *g_heap_start = NULL;
-static void *g_heap_end   = NULL;
+#define MAX_HEAP ((size_t)256 * 1024 * 1024) // 256 mb
+
+static char *g_heap_start = NULL;
+static char *g_heap_end   = NULL;
+static char *g_heap_cap   = NULL;
 
 int heap_init(void) {
-    (void)g_heap_start;
-    (void)g_heap_end;
-    return -1; /* stub */
+    void *p = mmap(NULL, MAX_HEAP, PROT_READ | PROT_WRITE,
+                   MAP_PRIVATE | MAP_ANON, -1, 0);
+    if (p == MAP_FAILED) return -1;
+    g_heap_start = p;
+    g_heap_end   = p;
+    g_heap_cap   = (char *)p + MAX_HEAP;
+    return 0;
 }
 
 void *heap_extend(size_t size) {
-    (void)size;
-    return NULL;
+    char *old_end = g_heap_end;
+    char *new_end = old_end + size;
+
+    if (new_end > g_heap_cap) return NULL;
+
+    g_heap_end = new_end;
+    return old_end;
 }
 
 void *heap_start(void) { return g_heap_start; }
